ac347ddf424fd969e8a77a13e503bb55a536ba85
liyhappy <59088119@qq.com>
2017-02-10 16:14:41 +0800

    Create HelloWorld.java

---
+++ src/main/java/com/nubia/app/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.nubia.app.helloworld;
+
+public class HelloWorld
+{
+    public String sayHello()
+    {
+  	return "hello nubia";
+    }
+    public static void main(String[] args)
+    {
+	System.out.print(new HelloWorld().sayHello());
+    }
+
+}
